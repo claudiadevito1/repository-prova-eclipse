13c59c8cd218b30063d02af882dc3e5734318e35
claudiadevito1 <devito.claudia1@gmail.com>
2023-02-01 15:01:36 +0100

    Modifica develop

---
+++ esercitazione.git.eclipse/src/esercitazione/git/eclipse/esercitazionetest.java
@@ -0,0 +1,10 @@
+package esercitazione.git.eclipse;
+
+public class esercitazionetest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world!");
+	}
+
+}
